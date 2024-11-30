be70270a84ca9b4912e95cc2b00466e76c217d2c
jlz9396 <jlz9396@gmail.com>
2024-11-29 22:06:59 -0600

    Update model

--- ml/model.py
+++ ml/model.py
@@ -125,12 +125,21 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
-    # TODO: implement the function
+    ##--- TODO: implement the function
+    sliced_data = data[data[column_name] == slice_value]
+
     X_slice, y_slice, _, _ = process_data(
+        sliced_data,
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb,
+    )
         # your code here
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
-    )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    
+    preds = inference(model, X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
